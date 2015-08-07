dca614dd92dc0688763b5e839a515352f6a52504
Carlos Rafael Giani <dv@pseudoterminal.org>
2015-08-07 09:13:35 +0200

    Add more tag_list helper functions
    Signed-off-by: Carlos Rafael Giani <dv@pseudoterminal.org>

--- nxplay/tag_list.cpp
+++ nxplay/tag_list.cpp
@@ -83,20 +83,16 @@ bool tag_list::is_empty() const
 }
 
 
-void tag_list::merge(tag_list const &p_other, GstTagMergeMode const p_merge_mode)
+void tag_list::insert(tag_list const &p_other, GstTagMergeMode const p_merge_mode)
 {
         // Safety check
         if (p_other.get_tag_list() == m_tag_list)
                 return;
 
-	GstTagList *new_tag_list = gst_tag_list_merge(m_tag_list, p_other.get_tag_list(), p_merge_mode);
+	if (m_tag_list == NULL)
+		m_tag_list = gst_tag_list_new_empty();
 
-        // Discard the old tag list
-	if (m_tag_list != NULL)
-		gst_tag_list_unref(m_tag_list);
-
-        // Adopt the new, merged tag list
-	m_tag_list = new_tag_list;
+	gst_tag_list_insert(m_tag_list, p_other.get_tag_list(), p_merge_mode);
 }
 
 
@@ -110,6 +106,147 @@ bool operator == (tag_list const &p_first, tag_list const &p_second)
 		return false;
 	else
 		return gst_tag_list_is_equal(p_first.get_tag_list(), p_second.get_tag_list());
+}
+
+
+void add_raw_value(tag_list &p_tag_list, GValue const *p_value, std::string const &p_name, GstTagMergeMode const p_merge_mode)
+{
+	g_assert(p_value != NULL);
+	g_assert(!p_name.empty());
+
+	if (p_tag_list.is_empty())
+		p_tag_list = tag_list(gst_tag_list_new_empty());
+
+	gst_tag_list_add_value(p_tag_list.get_tag_list(), p_merge_mode, p_name.c_str(), p_value);
+}
+
+
+bool has_value(tag_list const &p_tag_list, std::string const &p_name)
+{
+	g_assert(!p_name.empty());
+	return p_tag_list.is_empty() ? false : gst_tag_list_get_value_index(p_tag_list.get_tag_list(), p_name.c_str(), 0) != NULL;
+}
+
+
+guint get_num_values_for_tag(tag_list const &p_tag_list, std::string const &p_name)
+{
+	g_assert(!p_name.empty());
+	return p_tag_list.is_empty() ? 0 : gst_tag_list_get_tag_size(p_tag_list.get_tag_list(), p_name.c_str());
+}
+
+
+GValue const * get_raw_value(tag_list const &p_tag_list, std::string const &p_name, const guint p_index)
+{
+	g_assert(!p_name.empty());
+	return p_tag_list.is_empty() ? NULL : gst_tag_list_get_value_index(p_tag_list.get_tag_list(), p_name.c_str(), p_index);
+}
+
+
+#define GET_VALUE_IMPL(TYPE) \
+bool get_value(tag_list const &p_tag_list, std::string const &p_name, g ## TYPE &p_value, const guint p_index) \
+{ \
+	g_assert(!p_name.empty()); \
+	return p_tag_list.is_empty() ? false : gst_tag_list_get_ ## TYPE ## _index(p_tag_list.get_tag_list(), p_name.c_str(), p_index, &p_value); \
+}
+
+
+GET_VALUE_IMPL(int)
+GET_VALUE_IMPL(uint)
+GET_VALUE_IMPL(int64)
+GET_VALUE_IMPL(uint64)
+GET_VALUE_IMPL(float)
+GET_VALUE_IMPL(double)
+GET_VALUE_IMPL(pointer)
+
+
+bool get_value(tag_list const &p_tag_list, std::string const &p_name, GstSample* &p_value, const guint p_index)
+{
+	g_assert(!p_name.empty());
+	return p_tag_list.is_empty() ? false : gst_tag_list_get_sample_index(p_tag_list.get_tag_list(), p_name.c_str(), p_index, &p_value);
+}
+
+
+bool get_value(tag_list const &p_tag_list, std::string const &p_name, GDate* &p_value, const guint p_index)
+{
+	g_assert(!p_name.empty());
+	return p_tag_list.is_empty() ? false : gst_tag_list_get_date_index(p_tag_list.get_tag_list(), p_name.c_str(), p_index, &p_value);
+}
+
+
+bool get_value(tag_list const &p_tag_list, std::string const &p_name, GstDateTime* &p_value, const guint p_index)
+{
+	g_assert(!p_name.empty());
+	return p_tag_list.is_empty() ? false : gst_tag_list_get_date_time_index(p_tag_list.get_tag_list(), p_name.c_str(), p_index, &p_value);
+}
+
+
+bool get_value(tag_list const &p_tag_list, std::string const &p_name, std::string &p_value, const guint p_index)
+{
+	g_assert(!p_name.empty());
+
+	if (p_tag_list.is_empty())
+		return false;
+
+	gchar const *str;
+	gboolean ret = gst_tag_list_peek_string_index(p_tag_list.get_tag_list(), p_name.c_str(), p_index, &str);
+	if (!ret)
+		return false;
+
+	p_value = str;
+
+	return true;
+}
+
+
+tag_list calculate_new_tags(tag_list const &p_reference, tag_list const &p_other)
+{
+	tag_list result;
+	guint num_tags;
+
+	if (p_other.is_empty())
+		return result;
+
+	num_tags = gst_tag_list_n_tags(p_other.get_tag_list());
+	for (guint num = 0; num < num_tags; ++num)
+	{
+		std::string name(gst_tag_list_nth_tag_name(p_other.get_tag_list(), num));
+		guint num_tag_values_in_other = get_num_values_for_tag(p_other, name);
+
+		// If the reference list already has this tag, check if the values differ.
+		// If it doesn't have this tag yet, it is an addition, so add the tag to
+		// the result right away.
+		if (has_value(p_reference, name))
+		{
+			guint num_tag_values_in_reference = get_num_values_for_tag(p_reference, name);
+
+			// If the number of tag values differ, add the other's values to
+			// the result, assuming a change (an addition).
+			// If the numbers are the same, compare each value pair.
+			if (num_tag_values_in_other == num_tag_values_in_reference)
+			{
+				bool equal = true;
+
+				for (guint index = 0; index < num_tag_values_in_other; ++index)
+				{
+					GValue const *other_value = get_raw_value(p_other, name, index);
+					GValue const *reference_value = get_raw_value(p_reference, name, index);
+					if (gst_value_compare(other_value, reference_value) != GST_VALUE_EQUAL)
+					{
+						equal = false;
+						break;
+					}
+				}
+
+				if (equal)
+					continue;
+			}
+		}
+
+		for (guint index = 0; index < num_tag_values_in_other; ++index)
+			add_raw_value(result, get_raw_value(p_other, name, index), name, GST_TAG_MERGE_APPEND);
+	}
+
+	return result;
 }
 
 
--- nxplay/tag_list.hpp
+++ nxplay/tag_list.hpp
@@ -29,6 +29,10 @@ class tag_list
 {
 public:
 	/// Default constructor. Sets the internal GStreamer tag list pointer to null.
+	/**
+	 * To create a tag_list object with an empty taglist inside, call
+	 * tag_list(gst_tag_list_new_empty()) instead.
+	 **/
 	tag_list();
 	/// Copy constructor. Copies by using gst_tag_list_copy() internally.
 	tag_list(tag_list const &p_src);
@@ -65,10 +69,10 @@ public:
 	/// Returns true if the tag list is empty or the pointer is null.
 	bool is_empty() const;
 
-	/// Merge the other tag list into this one.
+	/// Inserts the other tag list into this one.
 	/**
 	 * The other tag list is left unchanged.
-	 * Internally, this merges by using gst_tag_list_merge().
+	 * Internally, this inserts by using gst_tag_list_insert().
 	 * if the other tag list object's pointer is the same as
 	 * the pointer of this object, this function does nothing.
 	 *
@@ -76,7 +80,7 @@ public:
 	 * @param p_merge_mode Merge mode to use in the internal
 	 *        gst_tag_list_merge() call
 	 */
-	void merge(tag_list const &p_other, GstTagMergeMode const p_merge_mode);
+	void insert(tag_list const &p_other, GstTagMergeMode const p_merge_mode);
 
 
 private:
@@ -101,6 +105,229 @@ inline bool operator != (tag_list const &p_first, tag_list const &p_second)
 {
 	return !(p_first == p_second);
 }
+
+/// Adds a raw GValue to the tag list.
+/**
+ * If the given tag list is empty, it will first fill it with a valid tag list.
+ * The value is copied and the copy added; this function does not take ownership
+ * over p_value.
+ *
+ * @param p_tag_list Tag list to add the value to
+ * @param p_value Non-NULL pointer to the GValue to be added (will be copied,
+ *        and the copy added)
+ * @param p_name Name of the tag to add
+ * @param p_merge_mode What to do if a tag with the given name already exists
+ */
+void add_raw_value(tag_list &p_tag_list, GValue const *p_value, std::string const &p_name, GstTagMergeMode const p_merge_mode);
+/// Returns true if a tag with the given name exists.
+/**
+ * @param p_tag_list Tag list to check
+ * @param p_name Name of the tag to look for
+ * @return true if a tag with the given name exists in the tag list, false if
+ *         no such tag is in the list, or if p_tag_list.is_empty() returns true
+ */
+bool has_value(tag_list const &p_tag_list, std::string const &p_name);
+
+/// Returns the number of values in the list for the given tag.
+/**
+ * @param p_tag_list Tag list to check
+ * @param p_name Name of the tag to count the values of
+ * @return Number of values for the given tag, or 0 if p_tag_list.is_empty()
+ *         returns true
+ */
+guint get_num_values_for_tag(tag_list const &p_tag_list, std::string const &p_name);
+/// Returns a pointer to the value for a given tag.
+/**
+ * Since multiple values can be present for the same tag,
+ * an additional integer index is needed. Minimum value is
+ * 0, maximum value is get_num_values_for_tag(p_tag_list, p_name)-1 .
+ *
+ * The return value is not copied. Do not try to unref or modify it.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_index Index of the tag value to get
+ * @return Const pointer to the value, or NULL if the index is out of bounds,
+ *         or no such tag with this name exists, or if p_tag_list.is_empty()
+ *         returns true
+ */
+GValue const * get_raw_value(tag_list const &p_tag_list, std::string const &p_name, const guint p_index);
+
+/// Returns a signed integer value for a given tag.
+/**
+ * This is a typesafe variant of get_raw_value(), added for convenience.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_value Where the tag value will be written to
+ * @param p_index Index of the tag value to get
+ * @return true if the tag is of this type, false if no such tag exists, or if
+ *         p_index is out of bounds, or if this tag is not of this type, or if
+ *         p_tag_list.is_empty() returns true
+ */
+bool get_value(tag_list const &p_tag_list, std::string const p_name, gint &p_value, const guint p_index);
+/// Returns an unsigned integer value for a given tag.
+/**
+ * This is a typesafe variant of get_raw_value(), added for convenience.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_value Where the tag value will be written to
+ * @param p_index Index of the tag value to get
+ * @return true if the tag is of this type, false if no such tag exists, or if
+ *         p_index is out of bounds, or if this tag is not of this type, or if
+ *         p_tag_list.is_empty() returns true
+ */
+bool get_value(tag_list const &p_tag_list, std::string const p_name, guint &p_value, const guint p_index);
+/// Returns a 64-bit signed integer value for a given tag.
+/**
+ * This is a typesafe variant of get_raw_value(), added for convenience.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_value Where the tag value will be written to
+ * @param p_index Index of the tag value to get
+ * @return true if the tag is of this type, false if no such tag exists, or if
+ *         p_index is out of bounds, or if this tag is not of this type, or if
+ *         p_tag_list.is_empty() returns true
+ */
+bool get_value(tag_list const &p_tag_list, std::string const p_name, gint64 &p_value, const guint p_index);
+/// Returns a 64-bit unsigned integer value for a given tag.
+/**
+ * This is a typesafe variant of get_raw_value(), added for convenience.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_value Where the tag value will be written to
+ * @param p_index Index of the tag value to get
+ * @return true if the tag is of this type, false if no such tag exists, or if
+ *         p_index is out of bounds, or if this tag is not of this type, or if
+ *         p_tag_list.is_empty() returns true
+ */
+bool get_value(tag_list const &p_tag_list, std::string const p_name, guint64 &p_value, const guint p_index);
+/// Returns a single-precision floating point value for a given tag.
+/**
+ * This is a typesafe variant of get_raw_value(), added for convenience.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_value Where the tag value will be written to
+ * @param p_index Index of the tag value to get
+ * @return true if the tag is of this type, false if no such tag exists, or if
+ *         p_index is out of bounds, or if this tag is not of this type, or if
+ *         p_tag_list.is_empty() returns true
+ */
+bool get_value(tag_list const &p_tag_list, std::string const p_name, gfloat &p_value, const guint p_index);
+/// Returns a double-precision floating point value for a given tag.
+/**
+ * This is a typesafe variant of get_raw_value(), added for convenience.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_value Where the tag value will be written to
+ * @param p_index Index of the tag value to get
+ * @return true if the tag is of this type, false if no such tag exists, or if
+ *         p_index is out of bounds, or if this tag is not of this type, or if
+ *         p_tag_list.is_empty() returns true
+ */
+bool get_value(tag_list const &p_tag_list, std::string const p_name, gdouble &p_value, const guint p_index);
+/// Returns a pointer value for a given tag.
+/**
+ * This is a typesafe variant of get_raw_value(), added for convenience.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_value Where the tag value will be written to
+ * @param p_index Index of the tag value to get
+ * @return true if the tag is of this type, false if no such tag exists, or if
+ *         p_index is out of bounds, or if this tag is not of this type, or if
+ *         p_tag_list.is_empty() returns true
+ */
+bool get_value(tag_list const &p_tag_list, std::string const p_name, gpointer &p_value, const guint p_index);
+/// Returns a pointer to a sample value for a given tag.
+/**
+ * This is a typesafe variant of get_raw_value(), added for convenience.
+ *
+ * The sample value is internally copied, and p_value is set to point to the copy.
+ * The result must be freed with gst_sample_unref() once it is no longer needed.
+ * See gst_tag_list_get_sample() for details.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_value Where the tag value will be written to
+ * @param p_index Index of the tag value to get
+ * @return true if the tag is of this type, false if no such tag exists, or if
+ *         p_index is out of bounds, or if this tag is not of this type, or if
+ *         p_tag_list.is_empty() returns true
+ */
+bool get_value(tag_list const &p_tag_list, std::string const p_name, GstSample* &p_value, const guint p_index);
+/// Returns a date value for a given tag.
+/**
+ * This is a typesafe variant of get_raw_value(), added for convenience.
+ *
+ * The date value is internally copied, and p_value is set to point to the copy.
+ * The result must be freed with g_date_free() once it is no longer needed.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_value Where the tag value will be written to
+ * @param p_index Index of the tag value to get
+ * @return true if the tag is of this type, false if no such tag exists, or if
+ *         p_index is out of bounds, or if this tag is not of this type, or if
+ *         p_tag_list.is_empty() returns true
+ */
+bool get_value(tag_list const &p_tag_list, std::string const p_name, GDate* &p_value, const guint p_index);
+/// Returns a datetime value for a given tag.
+/**
+ * This is a typesafe variant of get_raw_value(), added for convenience.
+ *
+ * The datetime value is internally copied, and p_value is set to point to the copy.
+ * The result must be freed with gst_date_time_unref() once it is no longer needed.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_value Where the tag value will be written to
+ * @param p_index Index of the tag value to get
+ * @return true if the tag is of this type, false if no such tag exists, or if
+ *         p_index is out of bounds, or if this tag is not of this type, or if
+ *         p_tag_list.is_empty() returns true
+ */
+bool get_value(tag_list const &p_tag_list, std::string const p_name, GstDateTime* &p_value, const guint p_index);
+/// Returns a string value for a given tag.
+/**
+ * This is a typesafe variant of get_raw_value(), added for convenience.
+ *
+ * @param p_tag_list Tag list to get the value from
+ * @param p_name Name of the tag to get a value of
+ * @param p_value Where the tag value will be written to
+ * @param p_index Index of the tag value to get
+ * @return true if the tag is of this type, false if no such tag exists, or if
+ *         p_index is out of bounds, or if this tag is not of this type, or if
+ *         p_tag_list.is_empty() returns true
+ */
+bool get_value(tag_list const &p_tag_list, std::string const p_name, std::string &p_value, const guint p_index);
+
+/// Calculates the tags that are new in p_other in comparison to p_reference.
+/**
+ * This helps with tag updates that also contain several already-reported tags.
+ * Suppose that p_reference contains a TITLE tag, and so does p_other.
+ * This function then checks if the TITLE tags in both lists are different, and if
+ * so, copies the TITLE tags from p_other into the output tag list. If p_other
+ * has a tag that p_reference doesn't, then this tag is added to the output list.
+ *
+ * If p_other and p_reference have both the same tag, but with a different number
+ * of values, the tag values from p_other are added to the output list. This helps
+ * with preserving the order of tag values in the list (which would potentially be
+ * lost if only the values that are different are extracted from p_other's tag
+ * values array).
+ *
+ * @param p_reference Reference tag list to compare p_other to
+ * @param p_other Extra tag list which will be compared to the reference one
+ * @return Tag list containing tags which are different between the two lists,
+ *         or present in p_other and missing in p_reference, or empty if there
+ *         are no differences
+ */
+tag_list calculate_new_tags(tag_list const &p_reference, tag_list const &p_other);
 
 /// Serializes the tag list to a string
 std::string to_string(tag_list const &p_tag_list);
